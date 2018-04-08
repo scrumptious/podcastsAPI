15f093e3ed29e0c6ef273768a2c72cd9dc68e616
scrumptious <ljasiurski@gmail.com>
2018-04-08 17:06:59 +0100

    create episode endpoint is checking now content-type of request and if episode exists already

--- src/Controller/EpisodeController.php
+++ src/Controller/EpisodeController.php
@@ -7,6 +7,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 //use Aws\S3\S3Client;
 use Aws\Sdk;
@@ -26,74 +28,140 @@ class EpisodeController extends Controller {
     $sdk = new Sdk($sharedConfig);
     $this->S3Client = $sdk->createS3();
     $this->bucketName = "jasiurski.cba.pl.test";
-
+    $this->request = Request::createFromGlobals();
+    $this->path = substr($this->request->getPathInfo(), 1);
+    $this->url = "https://s3.eu-west-2.amazonaws.com/" . $this->bucketName . $this->path;
   }
 
   /**
    * @Route("/{id}",
-   *   requirements={"id" = "\d+"}, defaults={"id" = 1})
+   *   requirements={"id" = "\d+"}, defaults={"id" = null})
    * @Method("POST")
    */
   public function create($podcast_id, $id) {
-    //if not exist create an episode with {id}
-    //otherwise return error http code
-    $this->keyFromParams = 'podcasts/' . $podcast_id . '/episodes/' . $id;
-    $this->url = "https://s3.eu-west-2.amazonaws.com/" . $this->bucketName . '/' . $this->keyFromParams;
-    $json = [
-      "Id" => 1,
-      "DownloadUrl" => $this->url,
-      "Title" => "podcast API task",
-      "Description" => "Following task is required to be completed in order to prove your top quality coding skills",
-      "EpisodeNumber" => $id,
-      "Created date" => "07-04-2018"
-    ];
-    $testData = json_encode($json);
-    $result = $this->S3Client->putObject([
-      'Bucket' => $this->bucketName,
-      'Key'    => $this->keyFromParams,
-      'Body'   => $testData,
-      'Content-Type' => "text/plain"
-    ]);
-    return new Response("result = " . $result);
+    if(!isset($id)) {
+      return new Response('No episode id specified', 400);
+    }
+    //serve the case of wrong content-type of request, expecting json
+    if($this->request->getContentType() === 'json') {
+      //check if episode doesn't exist yet
+      try {
+        $objects = $this->S3Client->getIterator('ListObjects', array('Bucket' => $this->bucketName, 'Key' => $podcast_id));
+      }
+      catch(S3Exception $e) {
+        return new Response("Could not list episodes", 400);
+      }
+      $subDirectory = 'podcasts/' . $podcast_id . '/episodes/';
+      foreach($objects as $object) {
+        if(strpos($id, substr($object['Key'], strlen($subDirectory))) !== false) {
+          //if exists inform with appropriate message and http code
+          return new Response("Episode already exist.", 409);
+        }
+      }
+      //otherwise upload episode data
+      $content = $this->request->getContent();
+      $result = $this->S3Client->putObject([
+        'Bucket' => $this->bucketName,
+        'Key'    => $this->path,
+        'Body'   => $content
+      ]);
+      if(isset($result)) {
+        return new Response("Episode created.", 201);
+      }
+    }
+    else {
+      return new Response("Wrong content-type, expecting 'application/json'", 415);
+    }
   }
 
   /**
    * @Route("/", name="episodes_list")
+   */
+  public function list($podcast_id) {
+    if($this->request->getContentType() === "json") {
+      $result = [];
+      try {
+        $objects = $this->S3Client->getIterator('ListObjects', array('Bucket' => $this->bucketName, 'Key' => $podcast_id));
+      }
+      catch(S3Exception $e) {
+        return new Response("Could not list episodes", 400);
+      }
+
+      $subDirectory = 'podcasts/' . $podcast_id . '/episodes/';
+      foreach($objects as $object) {
+        if(substr($object['Key'], 0, strlen($subDirectory)) === $subDirectory) {
+          array_push($result, substr($object['Key'], strlen($subDirectory)));
+        }
+      }
+      json_encode($result);
+      return new JsonResponse($result, 200);
+    }
+    else {
+      return new Response("Wrong content-type, expected 'application/json", 415);
+    }
+  }
+
+
+
+
+
+
+
+
+
+  /**
    * @Route("/{id}", name="episode", defaults={"id" = null})
    * @Method("GET")
    */
   public function show($podcast_id, $id = null) {
-    $this->keyFromParams = 'podcasts/' . $podcast_id . '/episodes/' . $id;
-
-    //if no id param passed, then list all episodes,
-    //otherwise show details about episode of id = {$id}
-    try {
-      $result = $this->S3Client->getObject([
-        'Bucket' => $this->bucketName,
-        'Key' => $this->keyFromParams,
-        'response-content-type' => "application/json"
-      ]);
-    } catch(S3Exception $e) {
-      echo 'Exception caught ' . $e->getMessage();
-    }
-    if(isset($result)) {
-      $jsonResult = json_decode($result['Body']);
-      return $this->render('podcast/show.html.twig', array(
-        'fromParams' => $this->keyFromParams,
-        'podcast_id' => $podcast_id,
-        'id' => $id,
-        'result' => $result['Body'],
-        'Id' => $jsonResult->Id,
-        'DownloadUrl' => $jsonResult->DownloadUrl,
-        'Title' => $jsonResult->Title,
-        'Description' => $jsonResult->Description,
-        'EpisodeNumber' => $jsonResult->EpisodeNumber
-      ));
+    if($this->request->getContentType() === 'json') {
+      //load object or return error
+      try {
+        $result = $this->S3Client->getObject([
+          'Bucket' => $this->bucketName,
+          'Key' => $this->path
+        ]);
+      } catch (S3Exception $e) {
+        if ($e->getAwsErrorMessage() === "The specified key does not exist.") {
+          return new Response($e->getAwsErrorMessage(), 404);
+        } else {
+          return new Response('', 400);
+        }
+      }
+      if (isset($result)) {
+        $jsonResult = $result['Body'];
+        //      return new Response($jsonResult, 200);
+        return new JsonResponse($jsonResult, 200, array(), true);
+      } else {
+        return new Response("Ups! Something went wrong", 404);
+      }
     }
     else {
-      return new Response("Ups! Something went wrong");
+      return new Response("Wrong content-type of request", 415);
     }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   /**
    * @Route("/{id}", name="delete_episode", defaults={"id" = null})
@@ -116,7 +184,7 @@ class EpisodeController extends Controller {
       return new Response("<p>Episode ". $this->id . "of " . $this->podcast_id . " podcast has been deleted.</p>");
     }
     else {
-      return new Response ("Could not delete anything", HTTP_, );
+      return new Response ("Could not delete anything");
     }
   }
 
